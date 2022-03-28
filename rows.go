c8e33557ca64d7b8684260de90a1d539f755225c
TAOI Masao <taoi_masao@taoi.jp>
2022-03-28 14:35:06 +0900

    本家にRowsColumnTypeDatabaseTypeNameを使えるプルリクを適用
    https://github.com/alexbrainman/odbc/pull/171

---
+++ rows.go
@@ -0,0 +1,80 @@
+// Copyright 2012 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package odbc
+
+import (
+	"database/sql/driver"
+	"io"
+
+	"github.com/taoikaihatsu-dev/odbc/api"
+)
+
+type Rows struct {
+	os *ODBCStmt
+}
+
+func (r *Rows) Columns() []string {
+	names := make([]string, len(r.os.Cols))
+	for i := 0; i < len(names); i++ {
+		names[i] = r.os.Cols[i].Name()
+	}
+	return names
+}
+
+func (r *Rows) Next(dest []driver.Value) error {
+	ret := api.SQLFetch(r.os.h)
+	if ret == api.SQL_NO_DATA {
+		return io.EOF
+	}
+	if IsError(ret) {
+		return NewError("SQLFetch", r.os.h)
+	}
+	for i := range dest {
+		v, err := r.os.Cols[i].Value(r.os.h, i)
+		if err != nil {
+			return err
+		}
+		dest[i] = v
+	}
+	return nil
+}
+
+func (r *Rows) Close() error {
+	return r.os.closeByRows()
+}
+
+func (r *Rows) HasNextResultSet() bool {
+	return true
+}
+
+func (r *Rows) NextResultSet() error {
+	ret := api.SQLMoreResults(r.os.h)
+	if ret == api.SQL_NO_DATA {
+		return io.EOF
+	}
+	if IsError(ret) {
+		return NewError("SQLMoreResults", r.os.h)
+	}
+
+	err := r.os.BindColumns()
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+// Implement RowsColumnTypeDatabaseTypeName interface in order to return column types.
+// https://github.com/golang/go/blob/e22a14b7eb1e4a172d0c20d14a0d2433fdf20e5c/src/database/sql/driver/driver.go#L469-L477
+//
+// From the docs:
+// RowsColumnTypeDatabaseTypeName may be implemented by Rows. It should return the
+// database system type name without the length. Type names should be uppercase.
+// Examples of returned types: "VARCHAR", "NVARCHAR", "VARCHAR2", "CHAR", "TEXT",
+// "DECIMAL", "SMALLINT", "INT", "BIGINT", "BOOL", "[]BIGINT", "JSONB", "XML",
+// "TIMESTAMP".
+func (rs *Rows) ColumnTypeDatabaseTypeName(i int) string {
+	return rs.os.Cols[i].DatabaseTypeName()
+
+}
